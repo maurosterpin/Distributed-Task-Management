359c2f23c819ed9082c59fdd1eb6355186f481ba
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-11-30 09:16:38 +0100

    Add get_task_by_params

--- main.py
+++ main.py
@@ -30,3 +30,27 @@ def get_task_by_id(task_id: int) -> Task:
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail=f"Task with id {task_id} does not exist")
     return tasks[task_id]
+
+@app.get("/tasks/")
+def get_task_by_params(
+    name: str | None = None, 
+    description: str | None = None, 
+    assigne: str | None = None,
+    owner: str | None = None, 
+    status: Status | None = None, 
+) -> dict[str, dict[str, str | Status | None]]:
+    def check_task(task: Task) -> bool:
+        return all(
+            (
+                name is None or task.name == name,
+                description is None or task.description == description,
+                assigne is None or task.assigne == assigne,
+                owner is None or task.owner == owner,
+                status is None or task.status is status
+            )
+        )
+    res = [task for task in tasks.values() if check_task(task)]
+    return {
+        "query": {"name": name, "description": description, "assigne": assigne, "owner": owner, "status": status},
+        "res": res
+    }
